7cd68432c97618e4f05f3a3881738b5e189f60f2
Matias Wouterlood <matiaswouterlood@yahoo.com.ar>
2019-12-09 22:11:49 +0100

    Registrarse

---
+++ TrabajoPractco.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="Practico.css" media="screen" title="no title" charset="utf-8">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Complete sus datos</h1>
+
+
+
+    <form>
+   <div class="form-row">
+     <div class="form-group col-md-6">
+       <label for="inputEmail4">Email</label>
+       <input type="email" class="form-control" id="inputEmail4">
+     </div>
+     <div class="form-group col-md-6">
+  </div>
+  <div class="form-roa">
+       <label for="inputPassword4">Contraseña</label>
+       <input type="password" class="form-control" id="inputPassword4">
+     </div>
+   </div>
+   <div class="form-grout">
+     <label for="inputAddress">Direccion</label>
+     <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+   </div>
+   <div class="form-grout">
+     <label for="inputAddress2">Detalles</label>
+     <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+   </div>
+   <div class="form-row">
+     <div class="form-group col-md-6">
+       <label class="lugar" for="inputCity">Ciudad</label>
+       <input type="text" class="form-control" id="inputCity">
+     </div>
+  </div>
+  <div>
+     <div class="form-group col-md-4">
+       <label class="stateee" for="inputState">Localidad</label>
+       <select id="inputState" class="form-control">
+         <option selected>Soy de...</option>
+         <option>...</option>
+       </select>
+     </div>
+
+   </div>
+   <div class="form-group">
+     <div class="form-check">
+       <input class="form-check-input" type="checkbox" id="gridCheck">
+       <label class="form-check-label" for="gridCheck">
+        Mantenerme conectado
+       </label>
+     </div>
+   </div>
+
+   <a class= "ingresa" href="#"> ¿ya tienes una cuenta? ingresa aqui</a>
+
+
+   <div class="signin">
+   <a href="">
+   <button type="submit" class="btn btn-primary">Registrarme</button>
+   </a>
+   </div>
+
+ </form>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
