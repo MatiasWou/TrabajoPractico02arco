3870a0f5beef452cd2437b4fa4faca27d88b0837
Matias Wouterlood <matiaswouterlood@yahoo.com.ar>
2019-12-10 18:44:16 +0100

    preguntas frecuentes

---
+++ PreguntasFrecuentes/Preguntas.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="estiloPreg.css">
+
+  <body style="background-color:#9658E6;">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+<div class="homeycosas">
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Perfil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">registrarme</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ingresar</a>
+      </li>
+    </ul>
+</div>
+
+<div>
+<h1><u>TIENDA ONLINE - PREGUNTAS FRECUENTES</u></h1>
+<div>
+  <div>
+     <ul class="lista">
+       <h2> Preguntas frecuentes:</h2>
+
+         <li>¿Donde los puedo encontrar?</li>
+         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+         <li>¿Cuáles son los métodos de pago disponibles?</li>
+         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+         <li>¿Qué tarjetas se aceptan para pagar los pedidos?</li>
+         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+         <li>¿Que Marcas de celular venden?</li>
+         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+         <li>¿Venden algun accesiorio?</li>
+         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+         <li>¿Puedo navegar por la web sin estar registrado?</li>
+         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+         <li>¿Puedo pedir la factura electrónica?</li>
+         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+
+       <h2>
+    Preguntas respecto a los pedidos:
+       </h2>
+
+         <li>¿Hacen envios?</li>
+           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit </p>
+         <li>¿Como hago un pedido?</li>
+           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit </p>
+         <li>¿Puedo hacer cambios en mi pedido?</li>
+           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit </p>
+         <li>¿Puede no haber disponibilidad de artículos en la compra online?</li>
+           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit </p>
+         <li>¿Cómo puedo saber si la Tienda sirve a mi domicilio?</li>
+           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit </p>
+         <li>¿Puedo añadir comentarios a mi pedido?</li>
+           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit </p>
+         <li>¿Cómo incluyo una dirección de envío?</li>
+           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit </p>
+         <li>¿Puedo cambiar la direccion de envio?</li>
+           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit </p>
+         <li>¿Puedo cambiar la hora/fecha de entrega?</li>
+           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit </p>
+
+     </ul>
+  </div>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
